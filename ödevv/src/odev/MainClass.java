958a98f9e558d3b9f889e035ea0a8295ba030027
RABİA ŞENLİK <55270779+123162@users.noreply.github.com>
2022-12-28 14:33:37 +0300

    Add files via upload

---
+++ ödevv/src/odev/MainClass.java
@@ -0,0 +1,19 @@
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import model.People;
+import model.Food;
+import model.Sport;
+
+
+public class MainClass {
+	
+	
+}
